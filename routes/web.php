29e94c06b76f72e4d7d65ae69ad28390c87c0ddb
y-choken <ch.apl.ln_yr@icloud.com>
2022-11-12 06:11:10 +0000

    PHP_Laravel_05

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\controllers\Admin\NewsController;
-Route::Controller(NewsController::class)->prefix('admin')->group(function() {
+ use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add');
 });
 
 
-
-Route::controller(AAAController::class)->group(function() {
-    Route::get('XXX/create', 'bbb');
-});
-
-
-
-use App\Http\controllers\Admin\ProfileController;
-Route::Controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
-});
+ use App\Http\Controllers\Admin\ProfileController;
+//  Route::Controller(ProfileController::class)->prefix('admin')->group(function() {
+    //  Route::get('profile/create', 'add');
+    //  Route::get('profile/edit', 'edit');
+//   });
